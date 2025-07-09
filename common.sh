f3a734d619963bf5cb46d93d88a47514663da5aa
sampath <sampathsvrk@gmail.coml>
2025-07-09 17:18:40 -0400

    Optimized the roboshop script

--- common.sh
+++ common.sh
@@ -159,7 +159,7 @@ system_setup(){
 }
 Print_time(){
 
-    END_TIME=(date+ %s)
+    END_TIME=$(date+ %s)
     TIME_TAKE=$(($END_TIME-$START_TIME))
     
     echo -e "Script execution completed successfully, ${Y}time taken : ${G}$TIME_TAKEN seconds $N" | tee -a $LOG_FILE
