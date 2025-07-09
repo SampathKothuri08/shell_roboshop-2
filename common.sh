86c3f8d133ac0a01861534dfbd07193d0ae31062
sampath <sampathsvrk@gmail.coml>
2025-07-09 17:03:47 -0400

    Optimized the roboshop script

--- common.sh
+++ common.sh
@@ -5,6 +5,7 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+USER_ID=$(id -u)
 LOGS_FOLDER="/var/log/roboshop-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
@@ -21,7 +22,6 @@ echo "script started at $(date)" | tee -a $LOG_FILE
 
 check_root()
 {
-    USER_ID=$(id -u)
 
     if [ $USER_ID -ne 0 ]
     then
